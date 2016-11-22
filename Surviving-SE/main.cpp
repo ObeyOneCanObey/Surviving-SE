d3c8882feafd4d5d16c9a726050f0eea359b537f
MindMatrix <JAYZ0N3@v1022-rn-156-81.campus-dynamic.uwaterloo.ca>
2016-11-22 04:09:07 -0500

    Initial Commit

---
+++ Surviving-SE/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Surviving-SE
+//
+//  Created by MindMatrix on 2016-11-22.
+//  Copyright © 2016 team26. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
